a4d841324f9ac6a1b02284ce7c7bdb06dacd1036
Daniel Lizardo <dslizardo@gmail.com>
2018-03-28 23:08:51 +0800

    Update cli argument for downloading manga, implement download manga method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from lxml import html
 import argparse
 import requests
+import re
 
 
 class Manga:
@@ -17,7 +18,8 @@ class Manga:
 
 HOST = 'https://readms.net'
 NEW_RELEASES_XPATH = '//div[@class="side-nav hidden-xs"]/ul[@class="new-list"]/li[@class="active"]/a'
-
+MANGA_PAGES_XPATH = '//div[@class="btn-group btn-reader-page"]/ul[@class="dropdown-menu"]/li'
+IMG_PAGE_XPATH = '//*[@id="manga-page"]'
 
 def get_new_releases():
     page=requests.get(HOST)
@@ -28,11 +30,36 @@ def get_new_releases():
         details=[t.strip() for t in r.itertext()]
         manga=Manga(r.get('href'),details[0],details[1],details[2],details[3])
         new_releases.append(manga)
-    return new_releases;
+    return new_releases
 
 
-def download_manga(name, chapter):
-    pass
+def get_manga_image(url):
+    page=requests.get(url)
+    tree=html.fromstring(page.content)
+    img_xpath=tree.xpath(IMG_PAGE_XPATH)
+    print(img_xpath)
+
+
+def download_manga(name):
+    page=requests.get(HOST)
+    tree=html.fromstring(page.content)
+    results=tree.xpath(NEW_RELEASES_XPATH+'[contains(text(),"'+name+'")]')
+    if len(results)==1:
+        manga=results[0]
+        manga_url=HOST+manga.get('href')
+
+        #Load first page
+        page=requests.get(manga_url)
+        tree=html.fromstring(page.content)
+        page_elements=tree.xpath(MANGA_PAGES_XPATH)
+        max_page=int(re.sub('[^0-9]','',page_elements[-1].text_content()))
+        for i in range(1,max_page+1):
+            manga_url=manga_url.rsplit('/',1)[0]+'/'+str(i)
+            get_manga_image(manga_url)
+
+
+    else:
+        print("No latest chapter for that manga")
 
 
 def display(mangas):
@@ -44,17 +71,16 @@ def display(mangas):
 if __name__ == "__main__":
     parser=argparse.ArgumentParser(description='CLI tool to check and download latest manga on readms.net',
                                      formatter_class=argparse.RawTextHelpFormatter)
-    help_desc="latest".ljust(15)+"- Get latest manga in readms"+"\ndownload".ljust(15)+" - Download manga; "
-    parser.add_argument('command', help=help_desc)
+    help_desc="latest".ljust(24)+"- Get latest manga in readms"+"\ndownload ${manga_name}".ljust(24)+" - Will download the latest chapter manga"
+    parser.add_argument('command', help=help_desc, nargs='+')
     args=parser.parse_args()
 
     command=args.command
-
-    if command=="latest":
+    if command[0] == "latest" and len(command) == 1:
         new_releases=get_new_releases()
         display(new_releases)
-    elif command=="download":
-        download_manga()
+    elif command[0] == "download" and len(command) == 2:
+        download_manga(command[1])
     else:
         parser.print_help()
 
